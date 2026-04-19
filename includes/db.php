d1ff69e705ca3b080d3598aac457c92eb3c696f0
veddegre <greg@vedders.com>
2026-04-18 21:31:07 -0400

    Remove email settings from db.php
    Removed email configuration settings from the database configuration.

--- includes/db.php
+++ includes/db.php
@@ -273,12 +273,6 @@ function seedDefaultSettings(PDO $pdo): void
         'subdomains_hidden'          => '0',
         'email_enabled'              => '0',
         'email_to'                   => '',
-        'email_from'                 => '',
-        'email_smtp_host'            => '',
-        'email_smtp_port'            => '587',
-        'email_smtp_user'            => '',
-        'email_smtp_pass'            => '',
-        'email_smtp_encryption'      => 'tls',
         'email_threshold'            => 'bot',
         'email_dedup_minutes'        => '60',
     ];
